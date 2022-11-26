75967938d5bc317260019da5b013503614150e0b
devilmanbabycry <zhukovsasha06@gmail.com>
2022-11-26 16:11:01 +0300

    v-4

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require('sequelize');
 
 module.exports = new Sequelize(
-    'telega_bot_db',
+    'nivent_bot',
     'root',
-    '',
+    'root',
     {
         host: '134.0.112.174',
         dialect: 'postgres',
