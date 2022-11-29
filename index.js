263c77055b9dbc2b888d288fbb6807b98f934e9b
devilmanbabycry <zhukovsasha06@gmail.com>
2022-11-29 15:25:37 +0300

    v-6-3-2

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ const start = async () => {
                 id: queryId,
                 title: 'Вы выбрали мероприятия!',
                 input_message_content: {
-                    message_text: `Выбрано мероприятий: ${totalPrice}\nМероприятия:\n${idEvent.map(item => "Название мероприятия: " + item.name + "\nСсылка на мероприятие: " + item.link + "\n" + "\n")}`
+                    message_text: `Выбрано мероприятий: ${totalPrice}\n\nМероприятия:\n${idEvent.map(item => "Название мероприятия: " + item.name + "\nСсылка на мероприятие: " + item.link + "\n" + "\n")}`
                 }
             })
             return res.status(200).json({});
