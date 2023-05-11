f030635ffddd3c0112f667149d28ce8eb4a1a3ef
devilmanbabycry <118069571+devilmanbabycry@users.noreply.github.com>
2023-05-11 14:35:20 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const {Sequelize} = require('sequelize');
 module.exports = new Sequelize(
     'nivent_bot',
     'postgres',
-    '',
+    'postgres ',
     {
         host: 'localhost',
         port: '5432',
